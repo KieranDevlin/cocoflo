8402f1d51fd23862928a8f0d82f09e6721401099
Kieran Devlin <kjrdevlin@gmail.com>
2020-05-25 16:37:40 -0700

    Updates

---
+++ precache-manifest.60872603dde64f7e34f3fc78b32d8afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b9e42af0bee9d2e441c650290bda2f",
+    "url": "/cocoflo/index.html"
+  },
+  {
+    "revision": "615fac01ca58bf0f83ee",
+    "url": "/cocoflo/static/css/main.bd263959.chunk.css"
+  },
+  {
+    "revision": "03fbdd67c779c92bd707",
+    "url": "/cocoflo/static/js/2.32936082.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cocoflo/static/js/2.32936082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615fac01ca58bf0f83ee",
+    "url": "/cocoflo/static/js/main.39c52786.chunk.js"
+  },
+  {
+    "revision": "dd0d9fa17396fe7a38fa",
+    "url": "/cocoflo/static/js/runtime-main.99d09943.js"
+  }
+]);
